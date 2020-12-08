9eb7550d6f4b2cb158012ef8f62f369a9b9d860b
ludoboulanger <ludovic.boulanger@outlook.com>
2020-12-07 20:46:59 -0500

    Implemented sticky header and restyled navbar

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,84 @@
-const toggleSideBar = () => {
-  const sidebar = document.getElementById('sidebar');
-  const iconPath = document.getElementById('navBarIconPath');
-  const icon = document.getElementById('navBarIcon')
+let isHeaderHidden = true;
+let isNavbarHidden = true;
 
-  if (sidebar.style.width === '') {
-    sidebar.style.width = '10rem';
-    sidebar.style.boxShadow = '-4px 0 7px black';
-    iconPath.style.fill = "gainsboro";
-    icon.style.position = 'fixed';
+// Fixed Header
+const moveHeaderText = () => {
+  const header = document.getElementById('header');
+
+  if (isNavbarHidden) {
+
+    header.classList.remove('header-move-right');
+    header.classList.add('header-move-left');
 
   } else {
-    sidebar.style.width = '';
-    iconPath.style.fill = " #252729";
-    icon.style.position = 'absolute';
+    header.classList.remove('header-move-left');
+    header.classList.add('header-move-right');
+  }
+
+  console.log(header.classList);
+};
+
+const createFixedHeader = () => {
+  const header = document.getElementById('header');
+  const iconPath = document.getElementById('navBarIconPath');
+
+  if (window.pageYOffset !== 0) {
+    isHeaderHidden = false;
+    toggleNavItemColor();
+    header.classList.remove('header-bar-fadeOut');
+    header.classList.add('header-bar-fadeIn');
+    header.style.boxShadow = '0 -4px 7px black';
+    iconPath.style.fill = "gainsboro";
+
+  } else {
+    isHeaderHidden = true;
+    toggleNavItemColor();
+    header.classList.remove('header-bar-fadeIn');
+    header.classList.add('header-bar-fadeOut');
+    iconPath.style.fill = "var(--text-dark)";
   }
 };
+
+const toggleNavItemColor = () => {
+  const navbar = document.getElementById('navbar-ul');
+
+  for (child of navbar.children) {
+    if (isHeaderHidden && !isNavbarHidden) {
+      console.log(child.children);
+      child.children[0].style.color = 'var(--text-dark)';
+    } else if (!isNavbarHidden) {
+      child.children[0].style.color = 'var(--text-light)';
+    }
+  }
+}
+
+const toggleNavbar = (e) => {
+  e.preventDefault();
+  const navbar = document.getElementById('navbar');
+  const iconPath = document.getElementById('navBarIconPath');
+
+  if (isNavbarHidden) {
+    isNavbarHidden = false;
+    toggleNavItemColor();
+    navbar.classList.remove('nav-bar-close');
+    navbar.classList.add('nav-bar-open');
+    navbar.style.fill = "gainsboro";
+
+  } else {
+    isNavbarHidden = true;
+    navbar.classList.remove('nav-bar-open');
+    navbar.classList.add('nav-bar-close');
+    if (window.pageYOffset === 0) {
+      iconPath.style.fill = "#252729";
+    } else {
+      iconPath.style.fill = "gainsboro";
+    }
+  }
+};
+
+// Listeners
+window.addEventListener('scroll', createFixedHeader);
+
+let icon = document.getElementById('hamburger-menu');
+
+icon.addEventListener('click', event => toggleNavbar(event));
